0e2bb0960b205c9d8c1cd450c205533860577e1a
Nikita Pepelyaev <termi364@mail.ru>
2023-03-04 16:59:55 +0400

    Add project files.

---
+++ CharMassiveFullAndFree/CharMassiveFullAndFree.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int const size = 15;
+	char buffer[size];
+
+	gets_s(buffer);
+
+	cout << "Количество элементов занято в массиве:" << strlen(buffer) << endl;
+
+	cout << "Количество оставшихся свободных элементов: " << size - strlen(buffer) << endl;
+
+	return 0;
+}
